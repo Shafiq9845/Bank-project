b95ce02caa83633307502de63def43a22db90be6
Veinal <veinalsequeira5229@gmail.com>
2024-12-26 17:06:52 +0530

    third commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -80,7 +80,7 @@ export default function Login() {
         className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-colors duration-200"
       >
         {/* <LogIn className="h-5 w-5 mr-2" /> */}
-        Sign in
+        Log in
       </button>
 
       {/* <div className="text-center">
