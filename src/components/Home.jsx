fc79cd51ffedb2d6713baf956239f4ccacee99e6
unknown <siddiqaboobakkarsiddiq.com>
2025-01-08 13:15:57 +0530

    some updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import NavBar from './NavBar'
-import Header from './Header'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div className='bg-blue-100 min-h-screen'>
-        {/* <div className=''>
-            <Header/>
-        </div>
-        <div className=''>
-            <NavBar/>
-        </div> */}
-
+   
         <div id="container" className='min-h-[calc(100vh-150px)] flex justify-center items-center'>
             <div id="box" className='border border-red-900 w-96 h-auto py-3 px-4 pb-4'>
               <ol className='mt-2 flex flex-col justify-center items-center gap-2'>
@@ -25,7 +17,7 @@ export default function Home() {
                 <li>Today's Memorandum ()</li>
                 <li>Current Month's Memorandum ()</li>
                 <Link to='#' className='text-blue-900 font-bold'><li>Document List</li></Link>
-                <Link to='#' className='text-blue-900 font-bold'><li>Docket List</li></Link>                
+                <Link to='/docketlistall' className='text-blue-900 font-bold'><li>Docket List</li></Link>                
                 <li className='font-bold'>Current Month's Master Circular ()</li>
                 <li className='font-bold'>Today's Master Circular ()</li>
               </ol>
